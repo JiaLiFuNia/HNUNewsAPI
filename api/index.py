31b86fcdcef2627fbddd8953e84df32e7fca45c2
JiaLiFuNia <woshixubohan04@163.com>
2024-02-18 11:34:27 +0800

    更新

--- api/index.py
+++ api/index.py
@@ -36,10 +36,11 @@ def json_data(limit):
         return data
 
 
-def geturl(url, limit):
+def geturl(url, limit, count):
     data = []
     pages = 8
     id_num = limit
+    ifBreak = 0
     if url == "":
         return data
     else:
@@ -72,50 +73,55 @@ def geturl(url, limit):
                 }
                 data.append(json_dict)
                 print(json_dict)
-                if id_num - limit == 100:
+                if count > 100 or count <= 0:
+                    count = 100
+                if id_num - limit == count:
+                    ifBreak = 1
                     break
+            if ifBreak == 1:
+                break
         return data
 
 
-def an():
+def an(count):
     url = 'https://www.htu.edu.cn/8955/list'
-    data = geturl(url, 1000)
+    data = geturl(url, 1000, count)
     code = 200
     message = '通知公告'
     app.json.ensure_ascii = False
     return code, message, data
 
 
-def bn():
+def bn(count):
     url = 'https://www.htu.edu.cn/8957/list'
-    data = geturl(url, 2000)
+    data = geturl(url, 2000, count)
     code = 200
     message = '院部动态'
     app.json.ensure_ascii = False
     return code, message, data
 
 
-def cn():
+def cn(count):
     url = 'https://www.htu.edu.cn/xsygcs/list'
-    data = geturl(url, 3000)
+    data = geturl(url, 3000, count)
     code = 200
     message = '学术预告'
     app.json.ensure_ascii = False
     return code, message, data
 
 
-def dn():
+def dn(count):
     url = 'https://www.htu.edu.cn/8954/list'
-    data = geturl(url, 4000)
+    data = geturl(url, 4000, count)
     code = 200
     message = '师大新闻'
     app.json.ensure_ascii = False
     return code, message, data
 
 
-def en():
+def en(count):
     url = 'https://www.htu.edu.cn/9008/list'
-    data = geturl(url, 5000)
+    data = geturl(url, 5000, count)
     code = 200
     message = '媒体师大'
     app.json.ensure_ascii = False
@@ -124,19 +130,20 @@ def en():
 
 @app.route("/<string:types>", methods=['get'])
 def home(types):
+    count = 0
     code = 201
     message = '非法请求'
     data = []
     if types == 'an':
-        code, message, data = an()
+        code, message, data = an(count)
     if types == 'bn':
-        code, message, data = bn()
+        code, message, data = bn(count)
     if types == 'cn':
-        code, message, data = cn()
+        code, message, data = cn(count)
     if types == 'dn':
-        code, message, data = dn()
+        code, message, data = dn(count)
     if types == 'en':
-        code, message, data = dn()
+        code, message, data = dn(count)
     if types == 'tabs':
         code = 200
         message = 'success'
@@ -158,31 +165,32 @@ def home(types):
         code = 200
         message = 'success'
         data = json_data(0)
-    return jsonify({'code': code, 'message': message, 'data': data})
+    return jsonify({'code': code, 'message': message, 'data': data, 'total': 100})
 
 
 @app.route('/', methods=['post'])
 def getnews():
     types = request.values.get("types")
+    count = int(request.values.get("count"))
     code = 201
     message = '非法请求'
     data = []
     if types == 'an':
-        code, message, data = an()
+        code, message, data = an(count)
     if types == 'bn':
-        code, message, data = bn()
+        code, message, data = bn(count)
     if types == 'cn':
-        code, message, data = cn()
+        code, message, data = cn(count)
     if types == 'dn':
-        code, message, data = dn()
+        code, message, data = dn(count)
     if types == 'en':
-        code, message, data = en()
+        code, message, data = en(count)
     if types == 'all':
         code = 200
         message = 'success'
         data = json_data(0)
     app.json.ensure_ascii = False
-    return jsonify({'code': code, 'message': message, 'data': data})
+    return jsonify({'code': code, 'message': message, 'data': data, 'total': count})
 
 
 @app.route("/", methods=['get'])
@@ -195,7 +203,8 @@ def getAllNews():
 
 
 def save_all_news():
-    data = an()[2] + bn()[2] + cn()[2] + dn()[2] + en()[2]
+    count = 0
+    data = an(count)[2] + bn(count)[2] + cn(count)[2] + dn(count)[2] + en(count)[2]
     json_dict = {'code': 200, 'message': 'success', 'data': data}
     with open("news.json", 'w', encoding='utf-8') as file:
         file.write(json.dumps(json_dict, ensure_ascii=False))
@@ -203,5 +212,5 @@ def save_all_news():
 
 
 if __name__ == '__main__':
-    save_all_news()
+    # save_all_news()
     app.run(debug=True)
